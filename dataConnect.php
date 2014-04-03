daaea24c3ab2b26e03b75954f07e099f2f9437d5
FloGro <grogne_f@etna-alternance.net>
2014-04-03 11:16:28 +0200

    Update dataConnect.php

--- dataConnect.php
+++ dataConnect.php
@@ -1,82 +1,166 @@
 <?php
-function dbConnect() {
-  $dbname = 'database.db';
-  $db = new SQLite3($dbname, 0666, $erreur);
-  if ($erreur != " ")
-    return ($db);
-  else
+require_once "dataConnect.php";
+
+function addUser($login, $email, $password) {
+  $db = dbConnect();
+  if ($db == FALSE)
+    return (FALSE);
+  $query = "insert into USER (login, email, password, created, modified, last_connexion) values \"".
+    $username."\",\"".$email."\",".md5($password)."\"\, date('now'), date('now'), date('now'));";
+  $result = $db->query($query);
+  if ($result == FALSE)
     {
-      echo "SQL error :" . $erreur;
+      dbClose($db);
       return (FALSE);
     }
-}
-function dbClose($db) {
-  $db->close();
-}
-function dbQuery($query) {
-  $db = dbConnect();
-  if ($db == FALSE)
-    return (0);
-  $result = $db->query($query);
   dbClose($db);
-  return ($result);
+  return (TRUE);
 }
-function dbSelectDisplay($query) {
+function addAdmin($login, $email, $password) {
+  $db = dbConnect();
+  if ($db == FALSE)
+    return (FALSE);
+  $query = "insert into USER (login, email, password, created, modified, last_connexion) values \"".
+    $username."\",\"".$email."\",".md5($password)."\"\, date('now'), date('now'), date('now'));";
+  $result = $db->query($query);
+  if ($result == FALSE)
+    {
+      dbClose($db);
+      return (FALSE);
+    }
+  dbClose($db);
+  return (TRUE);
+}
+function getUserID($login) {
   $db = dbConnect();
   if ($db == FALSE)
     return (0);
+  $query = "select id_user from USER where login like \"".$login."\";";
   $result = $db->query($query);
   while ($row = $result->fetchArray())
     {
       for ($i = 0; isset($row[$i]); $i++)
-	echo $row[$i] . " ";
-      echo "<br>";
+       $ID = $row[$i];
     }
   dbClose($db);
-  return (0);
+  if ($i > 1)
+    return (FALSE);
+  return ($ID);
 }
-function dbSelectToArray($query) {
+function getUserInfo($field, $ID) {
   $db = dbConnect();
   if ($db == FALSE)
     return (0);
-  $result = $db->query($query);
-  for ($j = 0 ;$row = $result->fetchArray(); $j++)
-    {
-      for ($i = 0; isset($row[$i]); $i++)
-        $array[$i][$j] = $row[$i];
-    }
-  dbClose($db);
-  return ($array);
-}
-function dbSearchDisplay($field_searched, $content, $table, $field_wanted) {
-  $db = dbConnect();
-  if ($db == FALSE)
-    return (0);
-  $query = "select \"".$field_wanted."\" from \"".$table."\" where \"".$field_searched
-    ."\" like \"".$content."\";";
+  $query = "select \"".$field."\" from USER where id_user like \"".$ID."\";";
   $result = $db->query($query);
   while ($row = $result->fetchArray())
     {
       for ($i = 0; isset($row[$i]); $i++)
-        echo $row[$i] . " ";
-      echo "<br>";
+	$info = $row[$i];
     }
   dbClose($db);
-  return (0);
+  if ($i > 1)
+    return (FALSE);
+  return ($info);
 }
-function dbSearchToArray($field_searched, $content, $table, $field_wanted) {
+function delUser($id) {
   $db = dbConnect();
   if ($db == FALSE)
     return (0);
-  $query = "select \"".$field_wanted."\" from \"".$table."\" where \"".$field_searched
-    ."\" like \"".$content."\";";
+  $query = "delete from USER where id_user like \"".$id."\";";
   $result = $db->query($query);
-  for ($j = 0; $row = $result->fetchArray(); $j++)
+  if ($result == FALSE)
     {
-      for ($i = 0; isset($row[$i]); $i++)
-        $array[$i][$j] = $row[$i];
+      dbClose($db);
+      return (FALSE);
     }
   dbClose($db);
-  return ($array);
+  return (TRUE);
 }
+function isUsernameExist($login){
+  $db = dbConnect();
+  if ($db == FALSE)
+    return (0);
+  $query = "select id_user from USER where login like \"".$login."\";";
+  $result = $db->query($query);
+  while ($row = $result->fetchArray())
+    {
+      for ($i = 0; isset($row[$i]); $i++)
+	$ID = $row[$i];
+    }
+  dbClose($db);
+  if ($i > 0)
+    return (TRUE);
+  return (FALSE);
+}
+function isEmailExist($email){
+  $db = dbConnect();
+  if ($db == FALSE)
+    return (0);
+  $query = "select id_user from USER where email like \"".$email."\";";
+  $result = $db->query($query);
+  while ($row = $result->fetchArray())
+    {
+      for ($i = 0; isset($row[$i]); $i++)
+        $ID = $row[$i];
+    }
+  dbClose($db);
+  if ($i > 0)
+    return (TRUE);
+  return (FALSE);
+}
+function userConnect($login, $password){
+  $db = dbConnect();
+  if ($db == FALSE)
+    return (0);
+  $id = getUserID($login);
+  $query = "select id_user from USER where id_user = \"".$id."\" and password = \"".md5($password)."\";";
+  $result = $db->query($query);
+  while ($row = $result->fetchArray())
+    {
+      for ($i = 0; isset($row[$i]); $i++)
+        $ID = $row[$i];
+    }
+  if ($i > 0)
+    {
+      $query = "update USER set last_connexion = date('now') where id_user = \"".$id."\";";
+      $result = $db->query($query);
+      dbClose($db);
+      return (TRUE);
+    }
+  dbClose($db);
+  return (FALSE);
+}
+function setUserField($id, $field, $newContent){
+  $db = dbConnect();
+  if ($db == FALSE)
+    return (FALSE);
+  $query = "update USER set \"".$field."\"=\"".$newContent."\" where id_user = \"".$id."\";";
+  $result = $db->query($query);
+  if ($result == FALSE)
+    {
+      dbClose($db);
+      return (FALSE);
+    }
+  $query = "update USER set modified = date('now') where id_user = \"".$id."\";";
+  $result = $db->query($query);
+  dbClose($db);
+  return (TRUE);
+}
+/*function isUserAdmin($id){
+  $db = dbConnect();
+  if ($db == FALSE)
+    return (0);
+  $query = "select id_user from USER where type = \"admin\" and id_user like \"".$id."\";";
+  $result = $db->query($query);
+  while ($row = $result->fetchArray())
+    {
+      for ($i = 0; isset($row[$i]); $i++)
+        $ID = $row[$i];
+    }
+  dbClose($db);
+  if ($i > 0)
+    return (TRUE);
+  return (FALSE);
+}*/
 ?>
