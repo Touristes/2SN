834b23296fd5ee5ea9d41cafa760e3f5286202e3
DemisBand <Tom@air-de-thomas.home>
2014-04-09 19:34:51 +0200

    graphique page profil (ebauche)

--- profil.php
+++ profil.php
@@ -1,9 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style2.css" />
+<meta charset="UTF-8">
+<title>Profil</title>
+</head>
 <?php
 include "sessionInit.php";
 require_once "dataUser.php";
 require_once "dataConnect.php";
 require_once "dataSubscriber.php";
+?>
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href='acc.php'><span>Home</span></a></li>
+   <li class='last'><a href='#'><span>Messages</span></a></li>    
+   <li><a href='profil.php'><span>Mon Profil</span></a></li>
+   <li class='last'><a href='deconnect.php'><span>Deconnexion</span></a></li>
+</ul>
+</div>
 
+<div id="infoprofil">
+<?php
 if(isset($_SESSION['login']))
   {
     $login = $_SESSION['login'];
@@ -65,17 +83,23 @@ if(isset($_SESSION['login']))
     }
     else {
     echo "Voici le compte-rendu de vos informations personnelles : ";
-    echo "<form method=\"POST\" action=\"profil.php\"><ul><li>User name : <input type=text name=login  value=\"".$login."\" /></li>";
-    echo "<li>E-Mail : <input type=text name=email  value=\"".getUserInfo("email",$id)."\" /></li>";
-    echo "<li>Nombre d'abonnes : ".getSubscriberNumber($id)."</li>";
-    echo "<li>Name : <input type=text name=name  value=\"".getUserInfo("name",$id)."\" /></li>";
-    echo "<li>First name : <input type=text name=firstname  value=\"".getUserInfo("firstname",$id)."\" /></li>";
-    echo "<li>Postal code : <input type=text name=postalcode value=\"".getUserInfo("postalcode",$id)."\" /></li>";
-    echo "<li><input type=submit value=\"Modifiez vos infos personnelles\"/></li>";
-    echo "</ul></form>";
-    echo "<form method=\"POST\" action=\"profil.php\"><input type=hidden name=changepasswd /><input type=submit value =\"Changer le mot de passe\" /></form>";
-    echo "<form method=\"POST\" action=\"profil.php\"><input type=hidden name=deluser /><input type=submit value =\"Effacer le compte\" /></form>";}
+	 echo "</br></br></br>Nombre d'abonnes : ".getSubscriberNumber($id);
+    echo "<form id=\"signup\" method=\"POST\" action=\"profil.php\"></br>User name : </br><input type=text name=login  value=\"".$login."\" />";
+    echo "</br>E-Mail : </br><input type=text name=email  value=\"".getUserInfo("email",$id)."\" />";
+    echo "</br>Name : </br><input type=text name=name  value=\"".getUserInfo("name",$id)."\" />";
+    echo "</br>First name : </br><input type=text name=firstname  value=\"".getUserInfo("firstname",$id)."\" />";
+    echo "</br>Postal code : </br><input type=text name=postalcode value=\"".getUserInfo("postalcode",$id)."\" />";
+    echo "</br><button type=submit value=\"Modifiez vos infos personnelles\"/>Modifiez vos infos personnelles</button>";
+    echo "</form>";
+    echo "</br></br></br></br><form id=\"signup\" method=\"POST\" action=\"profil.php\"><button type=submit value =\"Changer le mot de passe\">Changer le mot de passe</button></form>";
+    echo "</br><form id=\"signup\" method=\"POST\" action=\"profil.php\">
+</br>	</br></br><button type=submit value =\"Effacer le compte\">Effacer le compte</button></form>";}
   }
 else
   header('Location: index.php');
 ?>
+
+</div>
+<body>
+</body>
+</html>
