652ca98dafc4792c87871d888e9b0c75ef12c3c3
FloGro <grogne_f@etna-alternance.net>
2014-04-09 13:06:15 +0200

    Update dataUser.php
    AddUser complete et ajout de IsUserAdmin

--- dataUser.php
+++ dataUser.php
@@ -5,8 +5,37 @@ function addUser($login, $email, $password) {
   $db =dbConnect();
   if ($db == FALSE)
     return (FALSE);
-	$date = date("Y-m-d-H-i-s");
   $query = "INSERT INTO user (login, email, password, created, modified, last_connexion) values (\"".$login."\",\"".$email."\",\"".md5($password)."\",date('now'),date('now'),date('now'));";
+  $result = $db->query($query);
+  if ($result == FALSE)
+    {
+      dbClose($db);
+      return (FALSE);
+    }
+  $ID = getUserID($login);
+  $query = "INSERT INTO groups (name, id_user, created) values (\"User\",\"".$ID."\",date('now'));";
+  $result = $db->query($query);
+  if ($result == FALSE)
+    {
+      dbClose($db);
+      return (FALSE);
+    }
+  dbClose($db);
+  return (TRUE);
+}
+function addAdmin($login, $email, $password) {
+  $db =dbConnect();
+  if ($db == FALSE)
+    return (FALSE);
+  $query = "INSERT INTO user (login, email, password, created, modified, last_connexion) values (\"".$login."\",\"".$email."\",\"".md5($password)."\",date('now'),date('now'),date('now'));";
+  $result = $db->query($query);
+  if ($result == FALSE)
+    {
+      dbClose($db);
+      return (FALSE);
+    }
+  $ID = getUserID($login);
+  $query = "INSERT INTO groups (name, id_user, created) values (\"Admin\",\"".$ID."\",date('now'));";
   $result = $db->query($query);
   if ($result == FALSE)
     {
@@ -53,6 +82,13 @@ function delUser($id) {
   if ($db == FALSE)
     return (0);
   $query = "delete from user where id_user like \"".$id."\";";
+  $result = $db->query($query);
+  if ($result == FALSE)
+    {
+      dbClose($db);
+      return (FALSE);
+    }
+  $query = "delete from groups where id_user like \"".$id."\";";
   $result = $db->query($query);
   if ($result == FALSE)
     {
@@ -110,7 +146,6 @@ function userConnect($login, $password){
     }
   if ($i > 0)
     {
-		
       $query = "update user set last_connexion = date('now') where id_user = \"".$id."\";";
       $result = $db->query($query);
       dbClose($db);
@@ -135,5 +170,21 @@ function setUserField($id, $field, $newContent){
   dbClose($db);
   return (TRUE);
 }
-//function isUserAdmin($id);
+function isUserAdmin($id){
+  $db = dbConnect();
+  $i = 0;
+  if ($db == FALSE)
+    return (0);
+  $query = "select id_user from groups where name = \"Admin\" and id_user = \"".$id."\";";
+  $result = $db->query($query);
+  while ($row = $result->fetchArray())
+    {
+      for ($i = 0; isset($row[$i]); $i++)
+        $ID = $row[$i];
+    }
+  dbClose($db);
+  if ($i > 0)
+    return ("true");
+  return ("false");
+}
 ?>
