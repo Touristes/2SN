7c2107ff16e3dc197a1a66040120161c37ae0bd1
FloGro <grogne_f@etna-alternance.net>
2014-04-16 17:36:10 +0200

    Ajout de sessionDestroy.php + sessionInit.php

--- deconnect.php
+++ deconnect.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+include "sessionInit.php";
 	$_SESSION['check'] = "0";
 	include "sessionDestroy.php";
 	echo "<script type=\"text/javascript\">alert(\"Vous etes deconnecte !!\");location =\"co.php\"</script>";
