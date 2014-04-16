96849ac4186f2890c1af50bbdef75c31ef814143
FloGro <grogne_f@etna-alternance.net>
2014-04-16 17:32:18 +0200

    MAJ addSubscription, delSubscription, getSubs ...

--- dataSubscriber.php
+++ dataSubscriber.php
@@ -28,7 +28,7 @@ function delSubscription($id_subscriber, $id_user) {
   return (TRUE);
 }
 
-function getSubscriberNumber($id_user){
+function getSubscrptionNumber($id_user){
   $db = dbConnect();
   $i = 0;
   if ($db == FALSE)
@@ -43,7 +43,21 @@ function getSubscriberNumber($id_user){
   dbClose($db);
   return ($i);
 }
-
+function getSubscriberNumber($id_user){
+  $db = dbConnect();
+  $i = 0;
+  if ($db == FALSE)
+    return (0);
+  $query = "select id_user from subscriber where id_subscriber = \"".$id_user."\";";
+  $result = $db->query($query);
+  while ($row = $result->fetchArray())
+    {
+      for ($i = 0; isset($row[$i]); $i++)
+        $ID = $row[$i];
+    }
+  dbClose($db);
+  return ($i);
+}
 function isSubrscriberOf($id_subscriber, $id_user) {
   $db = dbConnect();
   $i = 0;
