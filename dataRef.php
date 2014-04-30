52c15ec5261314a720a6f3f51df39f97dc4b5bc2
FloGro <grogne_f@etna-alternance.net>
2014-04-30 13:27:04 +0200

    Correct bug github ...

--- dataRef.php
+++ dataRef.php
@@ -1,8 +1,8 @@
 <?php
-require_once "dataConnect.php";
-require_once "dataGroups.php";
-require_once "dataMessages.php";
-require_once "dataPost.php";
-require_once "dataSubscriber.php";
-require_once "dataUser.php";
+require_once "database/dataConnect.php";
+require_once "database/dataGroups.php";
+require_once "database/dataMessages.php";
+require_once "database/dataPost.php";
+require_once "database/dataSubscriber.php";
+require_once "database/dataUser.php";
 ?>
