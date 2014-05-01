ebcc11bd479176462740177fafce8e7415291202
raffin_j <divaskell@laposte.net>
2014-05-01 19:52:59 +0200

    Update send_post.php
    Remise en place des posts

--- send_post.php
+++ send_post.php
@@ -2,14 +2,16 @@
 
 include "dataRef.php";
 
+include "sessionInit.php";
+
 $post[2] = $_POST["posts"];
 $post[0] = $_SESSION["login"];
 $post[1] = $_POST["title"];
 $post[3] = 1; //category;
 $post[4] = 2; //type;
 
-echo "Il est vivant !!";
 addPost($post);
 
+header( 'Location: accueil.php' ) ;
 
 ?>
