88f1b89aeadd423f9e24bc38d6523a4e8729ad71
FloGro <grogne_f@etna-alternance.net>
2014-05-05 12:38:09 +0200

    Ajout de commentaires.

--- userProfilModel.php
+++ userProfilModel.php
@@ -1,3 +1,5 @@
+//Fonction qui renvoie un formulaire permettant de faire un utilisateur cliquable pour renvoyer vers son profil
+//Attention : ne doit pas être utilisée dans un formulaire existant.
 function profilLinkForm($login)
 {
   $linkForm = "<form id=\"formProfilView\" method=\"POST\" action=\"profilView.php\">"
@@ -5,7 +7,7 @@ function profilLinkForm($login)
                .$login."</button></form>";
   return ($linkForm);
 }
-
+//Fonction identique à celle ci-dessus sauf qu'elle doit être insérée dans un formulaire existant
 function profilLinkInForm($login)
 {
   $linkForm = "<button class=\"formProfilView\" type=\"submit\" name=\"loginProfilView\" value=\""
