1ea3ef79255ef0e9b184cfe512ad3c7018ce73c8
FloGro <grogne_f@etna-alternance.net>
2014-05-07 16:52:57 +0200

    Ajout des ids.

--- userProfilModel.php
+++ userProfilModel.php
@@ -4,15 +4,15 @@
 function profilLinkForm($login)
 {
   $linkForm = "<form id=\"formProfilView\" method=\"POST\" action=\"profilView.php\">"
-               ."<button class=\"formProfilView\" type=\"submit\" name=\"loginProfilView\" value=\"".$login."\">"
+               ."<button id=\"userLink\" class=\"formProfilView\" type=\"submit\" name=\"loginProfilView\" value=\"".$login."\">"
                .$login."</button></form>";
   return ($linkForm);
 }
 //Fonction identique à celle ci-dessus sauf qu'elle doit être insérée dans un formulaire existant
 function profilLinkInForm($login)
 {
-  $linkForm = "<button class=\"formProfilView\" name=\"loginProfilView\" value=\""
-                  .$login."\" onClick=\"this.form.action='profilView.php'; this.form.submit();\" >"                                                        
+  $linkForm = "<button id=\"userLink\" class=\"formProfilView\" name=\"loginProfilView\" value=\""
+                  .$login."\" onClick=\"this.form.action='profilView.php'; this.form.submit();\" >"                     
               .$login."</button>";
   return ($linkForm);
 }
