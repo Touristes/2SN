e35f9a541dc75f6a8ff01600f01dd5de19b98220
DemisBand <Tom@air-de-thomas.home>
2014-05-08 17:18:27 +0200

    Maj Contact Visu + Pop up d'envoi

--- contactForm.php
+++ contactForm.php
@@ -40,7 +40,7 @@ include "sessionInit.php";
 	<p>Un <a href="faq.php">problème sans solution</a> ? Des suggestions à faire pour l'amélioration de [Why] ? Ou peut-être souhaites-tu simplement nous déclamer ton amour d'une prose enflammée (en vers, on accepte aussi !) ? N'hésites pas à nous écrire, ton message sera lu !</p>
 	</br>
 	<div id="align">
-		<form action="contactForm.php" method="post">
+		<form id="signup" action="contactForm.php" method="post">
 			<ul>
 	<?php if ($_SESSION['check'] == "1"): ?>
 				<p>Ton pseudo [Why]: <input type="text" name="pseudo" placeholder="ex: SuperWhyId" value="<?php echo getUserInfo("login", getUserID($_SESSION['login'])); ?>" size="25"></p>
@@ -49,7 +49,7 @@ include "sessionInit.php";
 				<p>Ton nom : <input type="text" name="pseudo" placeholder="ex: SuperWhyId" style="width:241px;"></p>
 				<p>Ton e-mail : <input type="text" name="e-mail" placeholder="ex: e@mail.why" size="32"></p>
 	<?php endif; ?>
-				<li><select required="required" name="objet" method="post" action=""></li>
+				<li><select required name="objet" method="post" action=""></li>
 				<li><option value="default" disabled>Sélectionne l'objet de ton message</option></li>
 				<li><option value="connexion_pb">Problème de connexion</option></li>
 				<li><option value="press">Contact presse</option></li>
@@ -84,9 +84,9 @@ if (!empty($_POST)){
 	$message = $_POST['message'];
 
 	if (mail($to, $subject, $message))
-		echo "<p>Ton e-mail a peut-être été envoyé !</p>";
+	   echo "<script type=\"text/javascript\">alert(\"Ton e-mail a peut-être été envoyé !\");location =\"#\"</script>";
 	else
-		echo "<p>On dirait que ton mail n'est pas parti...</p>";
+		   echo "<script type=\"text/javascript\">alert(\"	On dirait que ton mail n'est pas parti...\");location =\"#\"</script>";
 }
 
 ?>
