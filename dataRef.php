cd5722dd7de8fecff17cbf50153a3934648ae136
FloGro <grogne_f@etna-alternance.net>
2014-05-19 17:30:48 +0200

    Update dataRef.php

--- dataRef.php
+++ dataRef.php
@@ -1,8 +1,13 @@
 <?php
+require_once "database/dataCategory.php";
 require_once "database/dataConnect.php";
+require_once "database/dataFile.php";
 require_once "database/dataGroups.php";
 require_once "database/dataMessages.php";
+require_once "database/dataPicture.php";
 require_once "database/dataPost.php";
+require_once "database/dataStat.php";
 require_once "database/dataSubscriber.php";
 require_once "database/dataUser.php";
+require_once "database/dataVideo.php";
 ?>
