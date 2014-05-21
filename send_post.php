d51509a9af7b7ff931823141577f13707a89c15e
FloGro <grogne_f@etna-alternance.net>
2014-05-21 15:26:25 +0200

    Update send_post.php

--- send_post.php
+++ send_post.php
@@ -1,15 +1,16 @@
 <?php
 
 include "dataRef.php";
-
 include "sessionInit.php";
 
 $post[2] = $_POST["posts"];
 $post[0] = $_SESSION["login"];
 $post[1] = $_POST["title"];
-$post[3] = 1; //category;
+$post[3] = getCategoryID("Text"); //category;
 $post[4] = 2; //type;
-
+if ($_FILES['file']) {
+  $post[3] = getCategoryID("Picture");
+}
 addPost($post);
 ?>
 <META http-EQUIV="Refresh" CONTENT="0; url=accueil.php">
