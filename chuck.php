2fc338ca94f6d7576d307af968b74c8df01a200b
FloGro <grogne_f@etna-alternance.net>
2014-05-22 16:55:30 +0200

    Ajout de callCaptainObvious

--- chuck.php
+++ chuck.php
@@ -108,4 +108,30 @@ function addTrollPic() {
 	echo "<img src=\"troll.jpg\" />";
 }
 
+function callCaptainObvious() {
+	$random = mt_rand(1, 20);
+	$array[1] = "Manger est le meilleur remède contre la faim.";
+	$array[2] = "Dans 365 jours, nous serons dans un an.";
+	$array[3] = "Vous êtes sur Why.";
+	$array[4] = "Vous êtes devant votre ordinateur";
+	$array[5] = "Demain est un autre jour";
+	$array[6] = "Quelqu’un dans le monde va acheter un calendrier dans les 12 prochains mois.";
+	$array[7] = "Le futur arrive.";
+	$array[8] = "Dans 2 ... non, trois ..., non attendez, dans des années, il y en aura une qui sera divisible par deux.";
+	$array[9] = "Dans 2 ans, nous serons 2 ans dans le futur.";
+	$array[10] = "Les gens morts ne sont pas vivants.";
+	$array[11] = "Si quelque chose devient difficile, ce ne sera plus facile.";
+	$array[12] = "Si vous detestez quelqu'un, ça indique probablement que vous ne l'aimez pas.";
+	$array[13] = "Quand vous avez soif, rien n'est meilleur que de boire liquide.";
+	$array[14] = "En haut, il y a la barre de menus.";
+	$array[15] = "Quand vous cliquez sur profil, vous allez sur la page profil.";
+	$array[16] = "Quand vous vous déconnetez, vous êtes déconnectés.";
+	$array[17] = "Vous êtes devant un écran.";
+	$array[18] = "Vous êtes sur un site web.";
+	$array[19] = "Quand vous cliquez sur messages, vous accedez à la partie message.";
+	$array[20] = "Votre écran est sale.";
+	//En ajouter plusieurs pour décrire les autres easter eggs
+	echo "<script type=\"text/javascript\">alert(\"".$array[$random]."\"); </script>";
+}
+
 ?>
