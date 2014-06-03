f1299d8541daa60999f3f2fb98cf9916e99a4eea
FloGro <grogne_f@etna-alternance.net>
2014-06-03 10:55:30 +0200

    reinitPassword Controler

--- reinitPassword.php
+++ reinitPassword.php
@@ -1,22 +1,5 @@
 <?php
-require_once "./Models/mail.php";
-require_once "./Controllers/frontControler.php";
-
-//Traitement du formulaire de renouvellement de mot de passe
-if (isset($_POST['reinitPassword'])) {
-    if ($_POST['mail'] == "") {
-      echo "<b>Veuillez renseigner une adresse mail.</b>";
-    }
-    else if (isEmailExist($_POST['mail']) == false) {
-      echo "<b>Veuillez renseigner une adresse mail valide.</b>";
-      }
-    else if (isEmailExist($_POST['mail']) == true) {
-      $id = getUserIDWithMail($_POST['mail']);
-      renewPassword($id);
-      //redirection a 10 sec apres envoi du mail
-      header('Refresh: 10; url=index.php');
-    }
-}
+include "./Controllers/reinitPasswordControler.php";
 ?>
 <!doctype html>
 <html>
