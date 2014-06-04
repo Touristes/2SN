ed738bb53ff482d34a5aa7b39981891e3ca0489c
raffin_j <raffin_j@etna-alternance.net>
2014-06-04 17:29:39 +0200

    demaon

--- database/deamon.php
+++ database/deamon.php
@@ -1,4 +1,4 @@
-#!/usr/local/bin/php
+#!/usr/bin/php
 <?php
 // deamon.php for deamon in /Users/raffin_j/raffin_j/2sn/2SN/database
 // 
@@ -6,13 +6,24 @@
 // Login   <raffin_j@etna-alternance.net>
 // 
 // Started on  Wed May 28 17:03:50 2014 jean-baptiste raffin
-// Last update Tue Jun  3 17:25:15 2014 jean-baptiste raffin
+// Last update Wed Jun  4 17:10:10 2014 jean-baptiste raffin
 //
-require_once 'dataConnect.php'
+chdir("../");
+require_once "./Controllers/dataControler.php";
 
-$date = date('Y-m-d');
-$week = mktime(0, 0, 0, date('Y'), date('m'), date('d')-7);
-dbConnect();
-$query = "DELETE * FROM post WHERE date BETWEEN '" + $week "' AND '" + $date + "';";
-dbquery($query);
-dbClose();
+$db = dbConnect();
+if ($db == FALSE)
+  return (0);
+$query = "select date('now', '-7 day');";
+$result = $db->query($query);
+while ($row = $result->fetchArray())
+  {
+	$time = $row[0];
+  }
+$delete_video = "DELETE FROM video where posted <='".$time."';";
+$db->query($delete_video);
+$delete_picture = "DELETE FROM picture where posted <= '".$time."';";
+$db->query($delete_picture);
+$delete_post = "DELETE FROM post WHERE created <= '".$time."';";
+$db->query($delete_post);
+dbClose($db);
