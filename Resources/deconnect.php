94419ba59e033887617cb1df17e319644a5b9f53
FloGro <grogne_f@etna-alternance.net>
2014-06-04 10:52:07 +0200

    Update deconnect.php

--- Resources/deconnect.php
+++ Resources/deconnect.php
@@ -1,6 +1,7 @@
 <?php
+chdir(../);
 include "./Resources/sessionInit.php";
 	$_SESSION['check'] = "0";
 	include "./Resources/sessionDestroy.php";
-	echo "<script type=\"text/javascript\">alert(\"Vous êtes déconnecté !!\");location =\"co.php\"</script>";
+	echo "<script type=\"text/javascript\">alert(\"Vous êtes déconnecté !!\");location =\"../co.php\"</script>";
 ?>
